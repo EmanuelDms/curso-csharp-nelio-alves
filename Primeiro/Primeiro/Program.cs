dd861771f41129f03018f5a57274b3176702fcd7
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-30 16:18:45 -0300

    chore: estrutura básica de um programa em c#

--- Primeiro/Primeiro/Program.cs
+++ Primeiro/Primeiro/Program.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿// Referência à namespace padrão do .NET para uso de funções básicas de manipulação de programas (BCL = Base Class Library)
+using System;
 
+// O namespace possui o mesmo nome do projeto, o que adquire essa sintaxe (Projeto (Assemblie) namespace)
+// boa prática é ficar desta forma
 namespace Primeiro
 {
+    // É convenção o nome da classe ser o mesmo do arquivo
     class Program
     {
+        // membro da classe estático (neste caso, um método Main())
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
