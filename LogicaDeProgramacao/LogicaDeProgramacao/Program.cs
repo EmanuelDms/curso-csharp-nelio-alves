3f0f3bc7bb325549d86cc35455ff068e58bc4b39
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-30 23:10:12 -0300

    feat: Saída de Dados + Tipos Básicos Csharp

---
+++ LogicaDeProgramacao/LogicaDeProgramacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace LogicaDeProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 10.38578;
+            Console.Write(saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            // placeholder
+            int idade = 32;
+            string nome = "Maria";
+
+            Console.WriteLine("\n{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+        }
+    }
+}
